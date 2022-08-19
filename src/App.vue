49ad9acc91d23712689ec62a8192c8207aacdc32
LinkunGao <gaolinkun123@gmail.com>
2022-08-19 21:29:59 +1200

    update base url

--- src/App.vue
+++ src/App.vue
@@ -41,7 +41,7 @@ onMounted(() => {
 
   gui = appRenderer.sceneInfos[0].gui;
   loadNrrd(
-    "/nrrd-segmentation/nrrd/breast-224.nrrd",
+    "/Nrrd-Segmentation/nrrd/breast-224.nrrd",
     "nrrd0",
     appRenderer.sceneInfos[0]
   );
@@ -58,7 +58,7 @@ function loadNrrd(url: string, name: string, sceneIn: Copper.copperMScene) {
     /**
      * for test 1 view
      * */
-    sceneIn.loadViewUrl("/nrrd-segmentation/nrrd_view.json");
+    sceneIn.loadViewUrl("/Nrrd-Segmentation/nrrd_view.json");
     sceneIn.subScene.add(nrrdMesh.z);
     sceneIn.dragImage(nrrdSlices.z, {
       mode: "mode1",
@@ -71,10 +71,10 @@ function loadNrrd(url: string, name: string, sceneIn: Copper.copperMScene) {
   };
   if (sceneIn) {
     sceneIn?.loadNrrd(url, funa);
-    sceneIn.loadViewUrl("/nrrd-segmentation/nrrd_view.json");
+    sceneIn.loadViewUrl("/Nrrd-Segmentation/nrrd_view.json");
   }
   sceneIn.updateBackground("#18e5a7", "#ff00ff");
-  Copper.setHDRFilePath("/nrrd-segmentation/venice_sunset_1k.hdr");
+  Copper.setHDRFilePath("/Nrrd-Segmentation/venice_sunset_1k.hdr");
   appRenderer.updateEnvironment(sceneIn);
 }
 
